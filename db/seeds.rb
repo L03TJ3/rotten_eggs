04c2c52144a5f38f68c09c9e91dba0cdb8a22cd7
frankdew <frankdew23@gmail.com>
2016-04-09 11:40:56 +0200

    Add seeds data films

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Film.create( { name: "The Shawshank Redemption", description: "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency." , rating: "", link: ""    } )
+Film.create( { name: "The Godfather", description: "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son." , rating: "", link: ""    } )
+Film.create( { name: "TThe Dark Knight", description: "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, the caped crusader must come to terms with one of the greatest psychological tests of his ability to fight injustice." , rating: "", link: ""    } )
+Film.create( { name: "Schindlers List", description: "In Poland during World War II, Oskar Schindler gradually becomes concerned for his Jewish workforce after witnessing their persecution by the Nazis." , rating: "", link: ""    } )
+Film.create( { name: "12 Angry Men", description: "
+A jury holdout attempts to prevent a miscarriage of justice by forcing his colleagues to reconsider the evidence." , rating: "", link: ""    } )
+Film.create( { name: "Pulp Fiction", description: "The lives of two mob hit men, a boxer, a gangster's wife, and a pair of diner bandits intertwine in four tales of violence and redemption." , rating: "", link: ""    } )
+Film.create( { name: "The Good, The Bad & The Ugly", description: "A bounty hunting scam joins two men in an uneasy alliance against a third in a race to find a fortune in gold buried in a remote cemetery." , rating: "", link: ""    } )
+Film.create( { name: "Fight Club", description: "An insomniac office worker, looking for a way to change his life, crosses paths with a devil-may-care soap maker, forming an underground fight club that evolves into something much, much more..." , rating: "", link: ""    } )
+Film.create( { name: "Goodfellas", description: "Henry Hill and his friends work their way up through the mob hierarchy." , rating: "", link: ""    } )
+Film.create( { name: "City of God", description: "Two boys growing up in a violent neighborhood of Rio de Janeiro take different paths: one becomes a photographer, the other a drug dealer." , rating: "", link: ""    } )
