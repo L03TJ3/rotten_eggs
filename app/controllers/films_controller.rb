c0dd96216dfcd47b8631947daee6cd700364809f
Laurence <laurenceweening@gmail.com>
2016-04-09 16:28:18 +0200

    Create new review

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -8,19 +8,5 @@ class FilmsController < ApplicationController
     @posts = @film.posts
   end
 
-  def new
-    @post = Post.new
-  end
 
-  def create
-    post_params = params.require( :post ).permit( :name, :title, :content)
-
-    @post = Post.new( post_params )
-
-    if @post.save
-      redirect_to 'new'
-    else
-      render 'new'
-    end
-  end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,26 @@
 class PostsController < ApplicationController
+  before_action :set_film
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    post_params = params.require( :post ).permit( :name, :title, :content, :film_id)
+
+    @post = Post.new( post_params )
+    @post.film = @film
+
+    if @post.save
+      redirect_to @film
+    else
+      render :new
+    end
+  end
+
+  protected
+
+  def set_film
+    @film = Film.find(params[:film_id])
+  end
 end
