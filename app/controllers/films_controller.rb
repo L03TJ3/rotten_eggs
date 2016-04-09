88b2fc4b250ac1b6a3ad516bf3e89ad5eb6423ce
Laurence <laurenceweening@gmail.com>
2016-04-09 14:47:13 +0200

    add posts data

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -5,6 +5,7 @@ class FilmsController < ApplicationController
 
   def show
     @films = Film.find( params[:id] )
+    @posts = Post.find( params[:id] )
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160409084818) do
+ActiveRecord::Schema.define(version: 20160409122844) do
 
   create_table "films", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 20160409084818) do
     t.string   "link"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "name"
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
